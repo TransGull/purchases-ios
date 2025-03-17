2d0820278f59392c823117f47a523d27b1f81599
noname <noname@local>
2025-03-17 12:46:38 +0800

    saywow overrides

--- Sources/Networking/HTTPClient/HTTPClient.swift
+++ Sources/Networking/HTTPClient/HTTPClient.swift
@@ -266,6 +266,9 @@ internal extension HTTPClient {
                 verificationMode: verificationMode,
                 internalSettings: internalSettings
             )
+            if let preferredLocales = httpRequest.preferredLocales {
+                self.headers["X-Preferred-Locales"] = preferredLocales
+            }
             self.verificationMode = verificationMode
 
             if let completionHandler = completionHandler {
